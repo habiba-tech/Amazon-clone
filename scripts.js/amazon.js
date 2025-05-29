0dc4bb52171093bb0230500ae6e2c5a26973aa54
habiba-tech <siddiquiummehabiba41@gmail>
2025-05-30 05:12:23 +0530

    html convert into javascript file

--- scripts.js/amazon.js
+++ scripts.js/amazon.js
@@ -1,3 +1,7 @@
+import {cart, addTocart } from '../data/cart.js';
+import { products } from '../data/products.js';
+
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -56,35 +60,25 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
+function updateCartquatity() {
+  let cartQuantity = 0;
 
-      let matchingItem;
-
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
-
-      let cartQuantity = 0;
-
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
+      cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
       });
 
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
+}
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      addTocart(productId);
+      updateCartquatity();
+
+     
+      
     });
   });
